1024e51fa3d1f0a288599cfaa249ee4811e98f09
Sammy Saglam <sammy@saglam.cc>
2022-01-19 07:16:04 +0000

    setup a remote schema

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,17 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { stitchSchemas } from "@graphql-tools/stitch";
 import { stitchingDirectives } from "@graphql-tools/stitching-directives";
+import { AsyncExecutor } from "@graphql-tools/utils";
+import { introspectSchema, wrapSchema } from "@graphql-tools/wrap";
+import { ApolloServer } from "apollo-server";
 import {
   ApolloServerPluginDrainHttpServer,
   ApolloServerPluginLandingPageGraphQLPlayground,
 } from "apollo-server-core";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer as ApolloExpressServer } from "apollo-server-express";
+import { fetch } from "cross-undici-fetch";
 import express from "express";
+import { print } from "graphql";
 import { PubSub } from "graphql-subscriptions";
 import { useServer } from "graphql-ws/lib/use/ws";
 import http from "http";
@@ -14,18 +19,19 @@ import { WebSocketServer } from "ws";
 
 const pubsub = new PubSub();
 
-const {
-  allStitchingDirectivesTypeDefs,
-  stitchingDirectivesValidator,
-  stitchingDirectivesTransformer,
-} = stitchingDirectives();
+(async () => {
+  const {
+    allStitchingDirectivesTypeDefs,
+    stitchingDirectivesValidator,
+    stitchingDirectivesTransformer,
+  } = stitchingDirectives();
 
-const db = {
-  users: [{ id: "1", name: "Sammy", favouriteProducts: ["1"] }],
-  products: [{ id: "1", name: "T-shirt" }],
-};
+  const db = {
+    users: [{ id: "1", name: "Sammy", favouriteProducts: ["1"] }],
+    products: [{ id: "1", name: "T-shirt" }],
+  };
 
-const productsTypeDefs = `
+  const productsTypeDefs = `
   ${allStitchingDirectivesTypeDefs}
 
   type Product {
@@ -51,78 +57,110 @@ type Mutation {
 
     # merge resolvers
     _sdl: String!
-    mergedUsers(ids: [ID!]!): [User!]! @merge(keyField: "id")
+    mergedUsers(ids: [ID!]!): [User!]!
   }
 `;
 
-const productsSchema = stitchingDirectivesValidator(
-  makeExecutableSchema({
-    typeDefs: productsTypeDefs,
+  const productsSchema = stitchingDirectivesValidator(
+    makeExecutableSchema({
+      typeDefs: productsTypeDefs,
+      resolvers: {
+        Query: {
+          products: () => db.products,
+
+          _sdl: () => productsTypeDefs,
+          mergedUsers: (root, { ids }) => ids.map((id: any) => ({ id })),
+        },
+        Mutation: {
+          sammy: () => {
+            pubsub.publish("POST_CREATED", {
+              sammy: "event!",
+            });
+
+            return "sammy";
+          },
+        },
+        Subscription: {
+          sammy: {
+            subscribe: () => pubsub.asyncIterator(["POST_CREATED"]),
+          },
+        },
+        User: {
+          favouriteProducts: () => db.products,
+          hello: () => "world",
+        },
+      },
+    }),
+  );
+
+  const productsServer = new ApolloServer({
+    schema: productsSchema,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  })
+    .listen({ port: 4001 })
+    .then(({ url }) => console.log(`running on ${url}`));
+
+  const executor: AsyncExecutor = async ({ document, variables }) => {
+    const query = print(document);
+    console.log(query);
+    const fetchResult = await fetch("http://localhost:4001/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query, variables }),
+    });
+    return fetchResult.json();
+  };
+
+  const remoteProductsSchema = wrapSchema({
+    schema: await introspectSchema(executor),
+    executor,
+  });
+
+  const usersSchema = makeExecutableSchema({
+    typeDefs: `
+      type User {
+        id: ID!
+        name: String
+      }
+
+      type Query {
+        users: [User!]!
+      }
+    `,
     resolvers: {
       Query: {
-        products: () => db.products,
+        users: () => db.users,
+      },
+    },
+  });
 
-        _sdl: () => productsTypeDefs,
-        mergedUsers: (root, { ids }) => ids.map((id: any) => ({ id })),
-      },
-      Mutation: {
-        sammy: () => {
-          pubsub.publish("POST_CREATED", {
-            sammy: "event!",
-          });
-
-          return "sammy";
-        },
-      },
-      Subscription: {
-        sammy: {
-          subscribe: () => pubsub.asyncIterator(["POST_CREATED"]),
-        },
-      },
+  const productsSubschema = {
+    schema: remoteProductsSchema,
+    merge: {
       User: {
-        favouriteProducts: () => db.products,
-        hello: () => "world",
+        fieldName: "mergedUsers",
+        selectionSet: "{ id }",
+        key: ({ id }: any) => id,
+        argsFromKeys: (ids: any) => ({ ids }),
       },
     },
-  }),
-);
+  };
+  const usersSubschema = {
+    schema: usersSchema,
+  };
 
-const usersSchema = makeExecutableSchema({
-  typeDefs: `
-    type User {
-      id: ID!
-      name: String
-    }
+  // build the combined schema
+  const gatewaySchema = stitchSchemas({
+    subschemaConfigTransforms: [stitchingDirectivesTransformer],
+    subschemas: [productsSubschema, usersSubschema],
+  });
 
-    type Query {
-      users: [User!]!
-    }
-  `,
-  resolvers: {
-    Query: {
-      users: () => db.users,
-    },
-  },
-});
-
-const productsSubschema = {
-  schema: productsSchema,
-};
-const usersSubschema = {
-  schema: usersSchema,
-};
-
-// build the combined schema
-const gatewaySchema = stitchSchemas({
-  subschemaConfigTransforms: [stitchingDirectivesTransformer],
-  subschemas: [productsSubschema, usersSubschema],
-});
-
-(async () => {
   const app = express();
   const httpServer = http.createServer(app);
 
-  const apolloServer = new ApolloServer({
+  const apolloServer = new ApolloExpressServer({
     schema: gatewaySchema,
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
